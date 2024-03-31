d719a714eb5f5ad7341c1ad1a4b759a27ce881d2
Kate_unmatched <KatyushenkaMarchenko@mail.ru>
2024-03-31 15:59:28 +0400

    Starting settings

---
+++ src/main/java/com/tsp/foxnight/FoxnightApplication.java
@@ -0,0 +1,13 @@
+package com.tsp.foxnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxnightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsp/foxnight/FoxnightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsp.foxnight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxnightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
