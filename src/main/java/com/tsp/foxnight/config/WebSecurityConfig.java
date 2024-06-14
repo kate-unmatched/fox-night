8fd2c80f960330c779fdbb7096ccb8af5087f21a
kate_killer <kate_unmatched@mail.ru>
2024-06-14 10:58:52 +0400

    Corse error when updating user

--- src/main/java/com/tsp/foxnight/config/WebSecurityConfig.java
+++ src/main/java/com/tsp/foxnight/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
